4c4f37ecfddea66d259db76ca3266b493a85d95d
Ares Ou <aresowj@gmail.com>
2016-03-21 21:11:43 -0700

    added some examples for loop

---
+++ CPP/CPP_Primer/chapter1/sum_50_to_100.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+int main()
+{
+    int sum = 0, val = 50;
+    // keep executing the while as long as val is less than or equal to 10
+    while ( val <= 100 ) {
+        sum += val;
+        ++val;
+    }
+
+    std::cout << "Sum of 5 to 100 inclusive is "
+              << sum << std::endl;
+    return 0;
+}
+
