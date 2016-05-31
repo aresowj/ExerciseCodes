a691143ae92ec84112780270175564e5b1d101e8
Ares Ou <aresowj@gmail.com>
2016-05-30 23:46:44 -0700

    first Swing example

---
+++ Java/head_first_design_pattern/observer_pattern/SwingObserverExample.java
@@ -0,0 +1,44 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class SwingObserverExample {
+    JFrame frame;
+
+    public static void main(String[] args) {
+        SwingObserverExample example = new SwingObserverExample();
+        example.go();
+    }
+
+    public void go() {
+        frame = new JFrame("Example");
+        JButton button = new JButton("Should I do it?");
+        button.addActionListener(new AngelListener());
+        button.addActionListener(new DevilListener());
+        frame.getContentPane().add(BorderLayout.CENTER, button);
+        
+        frame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent windowEvent) {
+                System.exit(0);
+            }
+        });
+        
+        // set frame properties here
+        frame.setSize(300, 300);
+        frame.setVisible(true);
+
+    }
+
+    class AngelListener implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
+            System.out.println("Don't do it, you might regret it!");
+        }
+    }
+
+    class DevilListener implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
+            System.out.println("Come on, do it!");
+        }
+    }
+}
+
