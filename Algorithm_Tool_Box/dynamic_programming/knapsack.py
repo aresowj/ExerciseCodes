e4e6293fa754e8b3559e7f2ee9f0c51ceb50156b
Ares Ou <aresowj@gmail.com>
2017-06-24 18:46:00 -0700

    Create knapsack.py

---
+++ Algorithm_Tool_Box/dynamic_programming/knapsack.py
@@ -0,0 +1,30 @@
+# Uses python3
+import sys
+
+def knapSack(W, wt, val, n):
+    K = [[0 for x in range(W+1)] for x in range(n+1)]
+ 
+    # Build table K[][] in bottom up manner
+    for i in range(n+1):
+        for w in range(W+1):
+            if i==0 or w==0:
+                K[i][w] = 0
+            elif wt[i-1] <= w:
+                K[i][w] = max(val[i-1] + K[i-1][w-wt[i-1]],  K[i-1][w])
+            else:
+                K[i][w] = K[i-1][w]
+ 
+    return K[n][W]
+
+
+def optimal_weight(max_weight, bars):
+    if not bars:
+        return 0
+    bars = [bar for bar in bars if bar <= max_weight]    
+
+    return knapSack(max_weight, bars, bars[:], len(bars))
+
+if __name__ == '__main__':
+    input = sys.stdin.read()
+    W, n, *w = list(map(int, input.split()))
+    print(optimal_weight(W, w))
