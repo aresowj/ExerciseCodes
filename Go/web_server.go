37634ddafe7579adf9c0cee8ffc0657bc49ffdec
Weijie Ou <ares.ou@icloud.com>
2025-08-08 21:07:04 -0700

    Update web_server.go to include image and complete html file

--- Go/web_server.go
+++ Go/web_server.go
@@ -3,14 +3,26 @@ package main
 import (
 	"fmt"
 	"net/http"
+    // "io"
+    "os"
+    // "bufio"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, 你好: %s\n", r.URL.Path)
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
 }
 
-func main() {
-	http.HandleFunc("/", handler)
 
+func main() {
+	dat, err := os.ReadFile("templates/index.html")
+	check(err)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, string(dat), r.URL.Query().Get("token"))
+	})
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 	http.ListenAndServe(":80", nil)
 }
